cddbc0e999518711ccf0305a81280dc51cb64dc8
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-01-09 04:48:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
     song_name =  filename.split(" - ")[1]  
 
     song = Song.new(song_name)
-    #song.artist_name = artist_name
+    song.artist_name = artist_name
     song
     
   end 
