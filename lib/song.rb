0bb81b245130444b446f4db978feeb0a1c5ea15d
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-01-09 04:35:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,15 +8,14 @@ class Song
   def self.new_by_filename(filename)
     artist_name = filename.split(" - ")[0] 
     song_name =  filename.split(" - ")[1]  
-    
+    artist_name = Artist.name
     song = Song.new(song_name)
     song
     
   end 
   
-  def self.artist_name=(artist_name)
-    artist_name = Artist.name
-    artist = Artist.find_or_create_by_name(artist_name) 
+  def self.artist_name=(name)
+    artist = Artist.find_or_create_by_name(name) 
     self.artist.name = artist
     artist.add_song(song)
     #artist.save
