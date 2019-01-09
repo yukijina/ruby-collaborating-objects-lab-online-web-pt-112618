b2d23f5edd28df304e08082bfd73cac240f75f28
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-01-09 04:40:40 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,6 +22,10 @@ class Artist
     @@all << self 
   end 
   
+  def self.find(name)
+    self.all.detect {|artist| artist.name == name} 
+  end 
+  
   def self.find_or_create_by_name(name) 
     if  self.all.detect {|artist| artist.name == name} 
       name
