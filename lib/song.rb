97d533aff8c35337c045d4179e56be1b224182b9
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-01-09 04:45:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
   
   def artist_name=(name)
     artist = Artist.find_or_create_by_name(name) 
-    self.artist.name = artist_name
+    self.artist = artist
     artist.add_song(song)
     #artist.save
     #artist.all
